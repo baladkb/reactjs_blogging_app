203d4bdd6db6c9c47310706d33340b12509770fc
praaveen <praaveen.ece@gmail.com>
2018-07-08 22:39:15 +0530

    signup with react routing

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # reactjs_blogging_app
+
 Creating a Blogging App Using React
 
-Refrence
+Reference
+
 a.https://code.tutsplus.com/tutorials/getting-started-with-react-and-jsx--cms-27352
-b.https://code.tutsplus.com/tutorials/creating-a-blogging-app-using-react-user-sign-in--cms-28568
-c.https://code.tutsplus.com/tutorials/creating-a-blogging-app-using-reactjs-add-post--cms-28579
+
+   https://gist.github.com/pvin/3e7fa5ab5b4424df86baff3cfd99b56c
+
+b.https://code.tutsplus.com/tutorials/understanding-how-to-handle-routing-in-react--cms-27355
+
+   https://github.com/pvin/reactRoutingApp
+
+c.https://code.tutsplus.com/tutorials/creating-a-blogging-app-using-react-user-sign-in--cms-28568
+
+d.https://code.tutsplus.com/tutorials/creating-a-blogging-app-using-reactjs-add-post--cms-28579
+
--- html/main.jsx
+++ html/main.jsx
@@ -45,6 +45,7 @@ class Signin extends React.Component {
 
     render(){
         return (
+            <div>
             <form className="form-signin">
 
                 <h2 className="form-signin-heading"> Please sign in </h2>
@@ -60,8 +61,60 @@ class Signin extends React.Component {
                 <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button"> Sign in </button>
 
             </form>
+            <div>
+                <Link to="/signup">{'Signup'}</Link>
+            </div>
+            </div>
         )
     }
 }
 
-ReactDOM.render( < Signin / > , document.getElementById('app'));
+class Signup extends React.Component{
+    render(){
+        return (
+            <div>
+                <form className="form-signin">
+
+                    <h2 className="form-signin-heading">Please sign up</h2>
+
+                    <label for="inputName" className="sr-only">Name</label>
+
+                    <input type="name" onChange={this.handleNameChange} id="inputName" className="form-control" placeholder="Name" required autofocus />
+
+                    <label for="inputEmail" className="sr-only">Email address</label>
+
+                    <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+
+                    <label for="inputPassword" className="sr-only">Password</label>
+
+                    <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+
+                    <button className="btn btn-lg btn-primary btn-block" onClick={this.signUp} type="button">Sign up</button>
+                </form>
+                <div>
+                    <Link to="/signin">{'SignIn'}</Link>
+                </div>
+            </div>
+        )
+    }
+
+    handleNameChange(e){
+
+    }
+
+    handleEmailChange(e){
+
+    }
+
+    handlePasswordChange(e){
+
+    }
+}
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route component={Signin} path="/"></Route>
+        <Route component={Signup} path="/signup"></Route>
+    </Router>,
+
+document.getElementById('app'));
